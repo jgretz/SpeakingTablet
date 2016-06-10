e2d9e5865c9a8421ef37043cb2e383c4c444bdc3
jgretz <jgretz@truefitsolutions.com>
2016-06-10 18:32:55 -0400

    initial creation of server and app

---
+++ client/index.ios.js
@@ -0,0 +1,4 @@
+import { SpeakingTablet } from './src/host';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('SpeakingTablet', () => SpeakingTablet);
---
+++ server/src/routes/test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  get: (req, res) => {
+    res.json({status:1});
+  }
+};
